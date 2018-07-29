e5e7115309504d574d7b33824d69dcad6abc8f4e
Rajkumar Purushothaman <rajkumar.purushothaman@lowes.com>
2018-07-28 23:26:08 -0400

    ui parts done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Container from "./components/Container";
+import Character from "./components/Character";
 import images from "./images.json";
 
 class App extends Component {
@@ -13,9 +14,11 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Header />
-        {this.state.images.map(image => (
-          <Container id={image.id} image={image.image} />
-        ))}
+        <Container>
+          {this.state.images.map(image => (
+            <Character key={image.id} image={image.image} />
+          ))}
+        </Container>
         <Footer />
       </div>
     );
---
+++ src/components/Character/Character.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Character.css";
+
+const Character = props => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+
+    {/* <div
+      role="img"
+      aria-label="click item"
+      class="click-item"
+      styles={{ backgroundImage: `url("/img/beth.png")` }}
+    /> */}
+
+    {/* <div className="content">
+      <ul>
+        <li>
+          <strong>Name:</strong> {props.name}
+        </li>
+        <li>
+          <strong>Occupation:</strong> {props.occupation}
+        </li>
+        <li>
+          <strong>Location:</strong> {props.location}
+        </li>
+      </ul>
+    </div> */}
+    {/* <span onClick={() => props.removeFriend(props.id)} className="remove">
+      𝘅
+    </span> */}
+  </div>
+);
+
+export default Character;
